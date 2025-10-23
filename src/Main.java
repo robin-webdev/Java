97d0c6f782e27085363c35b0ace64cf135065746
robin-webdev <robin.webdev.me@gmail.com>
2025-10-23 18:56:36 +0300

    just made this repo for java tutorial

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Hello World !");
+        char symbol = '$';
+        String name = "Robin";
+        boolean isStudent = true;
+        System.out.println(symbol);
+        System.out.println(name);
+        System.out.println(isStudent);
+    }
+}
