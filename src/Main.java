07025f63759513ddf2d200b70b5b665304dc31ef
robin-webdev <robin.webdev.me@gmail.com>
2025-11-01 23:39:33 +0200

    Modified a bit and made it ready for learning java removing classwork from this particular repo

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,64 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.print("Hello World !");
-        char symbol = '$';
-        String name = "Robin";
-        boolean isStudent = true;
-        System.out.println(symbol);
-        System.out.println(name);
-        System.out.println(isStudent);
+    public static void main(String[] args) {
+
+        /// Variables and print statements
+//        System.out.print("Hello World !");
+//        char symbol = '$';
+//        String name = "Robin";
+//        int age = 18;
+//        boolean isStudent = true;
+//        System.out.println(symbol);
+//        System.out.println(name);
+//        System.out.println(isStudent);
+
+
+        /// If else statements
+//        if (age < 18) {
+//            System.out.println("You are Minor");
+//        } else {
+//            System.out.println("You are Adult");
+//        }
+
+
+        /// Taking input from the user
+
+        Scanner newScanner = new Scanner(System.in);
+
+
+//        System.out.println(newScanner.hasNextLine());
+
+//        System.out.print("Enter your first Name : ");
+//        String firstName = newScanner.nextLine();
+//        System.out.print("Enter your Last Name : ");
+//        String lastName = newScanner.nextLine();
+//
+//        System.out.println("Your full name is " + firstName + " " + lastName);
+//
+        newScanner.close();  //remember to close the scanner after use
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
